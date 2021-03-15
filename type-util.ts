2db9443537e367d9b9551dface243d059a86840e
msl-ltd <msl-ltd@outlook.jp>
2021-03-16 01:30:28 +0900

    KeyOf入れ忘れとReplaceが分かりやすいので修正

--- type-util.ts
+++ type-util.ts
@@ -2,4 +2,4 @@ export type RecursivePartial<T> = { [P in keyof T]?: RecursivePartial<T[P]> };
 
 export type KeyOf<T> = { [P in keyof T]: T[P] };
 
-export type Override<T, P> = Omit<T, keyof P> & P;
+export type Replace<T, P> = KeyOf<Omit<T, keyof P> & P>;
