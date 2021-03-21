6042234121061d422d1841606dc54c2ee1c8a0fc
msl-ltd <msl-ltd@outlook.jp>
2021-03-21 17:46:53 +0900

    キャメルケースにし忘れを修正

--- create-instance.ts
+++ create-instance.ts
@@ -1,5 +1,5 @@
 type ClassType<T> = new (...argument: any[]) => T;
 
-export function CreateInstance<T>(classType: ClassType<T>, ...argment: any[]): T {
+export function createInstance<T>(classType: ClassType<T>, ...argment: any[]): T {
     return new classType(argment);
 }
